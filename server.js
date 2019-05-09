d24f9aa5ba46d0317eb77ddd83ea218c3730033a
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-09 13:22:52 +0000

    working w/ mongoDB

--- server.js
+++ server.js
@@ -50,43 +50,6 @@ app.get("/data", function(req, res) {
 });
 
 
-//---get request for data in mongoDB / mongo shell
-
-// app.get('/data', function (req, res) {
-
-//     // Grab the user input from the url query and assign it to a variable
-//     const keyword = req.query.keyword;
-
-    
-//     app.get('/data', function (req, res) {
-
-//     const keyword = req.query.keyword;
-
-
-//     Word.find({ word: keyword }, function (error, word) {
-//         // Because we are searching an array of objects, adding the array position is needed
-//         if (keyword===keyword) {
-//             res.send(`YUP! the word: <h4>"${keyword}"</h4> does exist within the dictionary!`);
-//         } else {
-//             res.send(`Sorry! But the word: <h3>"${keyword}"</h3> does not exist within the dictionary!`);
-//         }
-//     });
-// });
-
-
-
-
-// app.get('/data', function(req, res){
-//     const keyword = req.query.keyword;
-    
-//     Word.find ({}, function (error, words){
-//         if (Word.find ({word : keyword}) === keyword){
-//             res.send(`YUP! the word: <h4>"${keyword}"</h4> does exist within the dictionary!`);
-//         }
-//     })
-    
-// })
-
 
 
 
