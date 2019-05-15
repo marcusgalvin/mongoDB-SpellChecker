3ce983bc1df1dea249904e58a6304c680ea5d699
Marcus Galvin <marcusgalvin.student@careerdevs.com>
2019-05-15 20:53:06 +0000

    server

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 const bodyParser = require('body-parser');
 
 
+
 //---Init Mongoose
 const mongoose = require('mongoose');
+ let Schema = mongoose.Schema;
 
 
 //---Pull in the dictionary data file
@@ -27,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
  mongoose.connect('mongodb://'+ process.env.IP + '/spellchecker', {useNewUrlParser: true})  
      .then(()=>console.log("mongoDB connected"));
      
-     
+    
 
 //---schema
 const mySchema = new mongoose.Schema({
@@ -35,13 +37,19 @@ const mySchema = new mongoose.Schema({
 });
 
 //---model
-const Word = mongoose.model("Word", mySchema);
+const word = mongoose.model("word", mySchema);
+
+// console.log(word)
+
+// let one = word.find({})
+// console.log(one)
+
 
 
 //---get request  ---- working w/ mongoDB
 app.get("/data", function(req, res) {
     const keyword = req.query.word;
-    if(Word.hasOwnProperty(keyword)){
+    if(data.hasOwnProperty(keyword)){
         res.send(`YUP! the word: <h4>"${keyword}"</h4> does exist within the dictionary!`);
     }else{
         res.send(`Sorry! But the word: <h3>"${keyword}"</h3> does not exist within the dictionary!`);
@@ -53,9 +61,60 @@ app.get("/data", function(req, res) {
 
 
 
+
 //---PORT listener
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
+
+       
+
+
+
+
+
+
+//---get request for data in mongoDB / mongo shell
+
+// app.get('/data', function (req, res) {
+
+//     // Grab the user input from the url query and assign it to a variable
+//     const keyword = req.query.keyword;
+
+    
+//     app.get('/data', function (req, res) {
+
+//     const keyword = req.query.keyword;
+
+
+//     Word.find({ word: keyword }, function (error, word) {
+//         // Because we are searching an array of objects, adding the array position is needed
+//         if (keyword===keyword) {
+//             res.send(`YUP! the word: <h4>"${keyword}"</h4> does exist within the dictionary!`);
+//         } else {
+//             res.send(`Sorry! But the word: <h3>"${keyword}"</h3> does not exist within the dictionary!`);
+//         }
+//     });
+// });
+
+
+
+
+// app.get('/data', function(req, res){
+//     const keyword = req.query.keyword;
+    
+//     Word.find ({}, function (error, words){
+//         if (Word.find ({word : keyword}) === keyword){
+//             res.send(`YUP! the word: <h4>"${keyword}"</h4> does exist within the dictionary!`);
+//         }
+//     })
+    
+// })
+
+
+
+
+//---PORT listener
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
 
